aeec3200fc6821bbe8d3c886b0cd78841a9716e6
ardianrez <titrasi@gmail.com>
2022-04-04 22:31:40 +0800

    ok

--- app.py
+++ app.py
@@ -3,5 +3,12 @@ import connection
 if __name__ == "__main__":
     path = os.getcwd()
     path_query = path + '/sql/'
+    file_query = 'dml_query_1.sql'
     conn = connection.db_connect()
-    query = 
+    cur=conn.sursor()
+
+    with open(path_query + file_query,'r') as file:
+        query = file.read()
+
+    cur.execute(query)
+    cur.fetchall()
